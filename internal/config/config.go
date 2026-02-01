f6e38f9f6eb79b4f9d249a8cae4e49618b0e3db8
svnnj <svnnj@proton.me>
2026-02-01 21:32:06 +0300

    env cfg and tests

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,20 +2,30 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type Config struct {
-	Host          string
-	ShortenedHost string
+	ServerAddress string
+	BaseURL       string
 }
 
 func Get() Config {
-	host := flag.String("a", "localhost:8080", "main server host")
-	shortenedHost := flag.String("b", "http://localhost:8080", "shortened URL server host")
+	var cfg Config
+
+	cfg.ServerAddress = os.Getenv("SERVER_ADDRESS")
+	cfg.BaseURL = os.Getenv("BASE_URL")
+
+	serverAddressFlag := flag.String("a", "localhost:8080", "main server host")
+	baseURLFlag := flag.String("b", "http://localhost:8080", "shortened URL server host")
 	flag.Parse()
 
-	return Config{
-		Host:          *host,
-		ShortenedHost: *shortenedHost,
+	if cfg.ServerAddress == "" {
+		cfg.ServerAddress = *serverAddressFlag
 	}
+	if cfg.BaseURL == "" {
+		cfg.BaseURL = *baseURLFlag
+	}
+
+	return cfg
 }
